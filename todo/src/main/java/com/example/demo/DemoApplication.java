96bdc4bb276488fdf1151faee19cf17e8e7c9c94
Hery Nirintsoa <hhery889@gmail.com>
2024-05-22 16:54:13 +0300

    fix: docker maven cache

--- todo/src/main/java/com/example/demo/DemoApplication.java
+++ todo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
